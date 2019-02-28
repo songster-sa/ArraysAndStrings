444b8f1cc3cd1e50eb490653ee0c113601abe352
songster-sa <songster.sa@gmail.com>
2019-03-01 06:24:37 +1100

    add misc bank

---
+++ src/main/onlineBank/MagicOf3.java
@@ -0,0 +1,15 @@
+package main.onlineBank;
+
+/**
+ * A number ending with 3 will have a multiple which is all 1s
+ * Example:
+ * 111 is a multiple of 3
+ * 111111 is a multiple of 13
+ * <p>
+ * Given a number ending with 3 find its least multiple which is all 1s
+ * <p>
+ * Catch : the multiple may be beyond the range of int / log etc.
+ * Catch : optimize for time
+ */
+public class MagicOf3 {
+}
